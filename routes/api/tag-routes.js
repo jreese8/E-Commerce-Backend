b8957dacc0ad1444fcc9733694eaab651b048cba
jreese8 <johnathanreese8@gmail.com>
2022-03-07 13:12:11 -0500

    Fixed an error in tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -77,7 +77,7 @@ router.delete('/:id', (req, res) => {
         res.status(404).json({ message: 'No tag found with this id' });
         return;
       }
-      res.json(dbCategoryData);
+      res.json(dbTagData);
     })
     .catch(err => {
       console.log(err);
